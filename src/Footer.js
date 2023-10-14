8cfd62e7710ba72d5b40fb64a44f0ee546537bc4
AJITH S <128063551+Ajith0405@users.noreply.github.com>
2023-10-14 10:50:45 +0530

    second commit

--- src/Footer.js
+++ src/Footer.js
@@ -6,10 +6,10 @@ import './css/footer.css'
 function Footer() {
     return (
         <div>
-            <div className='me-3'>
+            <div className='container'>
                 <div className='row'>
-                    <div className='col-12 col-lg-4'>
-                        <div className='d-flex justify-content-center'>
+                    <div className='col-12 col-lg-3 col-sm-3 col-md-3'>
+                        <div className=''>
                             <div>
                                 <h3 className=''>Jadoo.</h3>
                                 <p className=''>Book your trip in minute, get full <br /> control for much longer.</p>
@@ -18,7 +18,7 @@ function Footer() {
                         </div>
 
                     </div>
-                    <div className='col-12 col-lg-2'>
+                    <div className='col-12 col-lg-2 col-sm-2 col-md-2'>
 
                         <ul>
                             <h4>Company</h4>
@@ -33,7 +33,7 @@ function Footer() {
                             </li>
                         </ul>
                     </div>
-                    <div className='col-12 col-lg-2'>
+                    <div className='col-12 col-lg-2 col-sm-2 col-md-2'>
 
                         <ul>
                             <h4>Contact</h4>
@@ -48,7 +48,7 @@ function Footer() {
                             </li>
                         </ul>
                     </div>
-                    <div className='col-12 col-lg-2'>
+                    <div className='col-12 col-lg-2 col-sm-2 col-md-2'>
 
                         <ul>
                             <h4>More</h4>
@@ -63,7 +63,7 @@ function Footer() {
                             </li>
                         </ul>
                     </div>
-                    <div className='col-12 col-lg-2'>
+                    <div className='col-12 col-lg-3 col-sm-2 col-md-3'>
                         <div className='mt-2 text-center'>
                             <i className="fa-brands fa-facebook-f me-2" style={{ padding: '15px', borderRadius: '50%', color: 'black', boxShadow: 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px' }}></i>
                             <i className="fa-brands fa-instagram me-2 fa-brands-active" style={{ padding: '15px', borderRadius: '50%', color: 'black', boxShadow: 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px' }} ></i>
@@ -74,22 +74,22 @@ function Footer() {
                         </div>
                         <div className='d-flex justify-content-center'>
                             <div className='d-flex'>
-                                <div className='d-flex align-items-center' style={{ padding: '10px', borderRadius: '40px', backgroundColor: 'black' }}>
+                                <div className='d-flex align-items-center me-2' style={{ padding: '10px', borderRadius: '40px', backgroundColor: 'black' }}>
                                     <div className='me-2' >
                                         <img className='' src={goole} alt='Play store' />
                                     </div>
                                     <div>
-                                        <p className='my-auto' style={{ fontSize: '10px' }}><span style={{ fontWeight: 'bold', color: '#ffff' }}>GET IT ON </span><br /><span style={{ color: 'gray' }}>GOOGLE PLAY</span></p>
+                                        <p className='my-auto' style={{ fontSize: '8px' }}><span style={{ fontWeight: 'bold', color: '#ffff' }}>GET IT ON </span><br /><span style={{ color: 'gray' }}>GOOGLE PLAY</span></p>
                                     </div>
 
 
                                 </div>
-                                <div className='d-flex align-items-center' style={{ padding: '10px', borderRadius: '40px', backgroundColor: 'black', border: '1px solid yellow' }}>
+                                <div className='d-flex align-items-center' style={{ padding: '10px', borderRadius: '40px', backgroundColor: 'black'}}>
                                     <div className='me-2' >
                                         <img className='' src={apple} alt='Play store' />
                                     </div>
                                     <div>
-                                        <p className='my-auto' style={{ fontSize: '10px' }}><span style={{ color: 'gray' }} >Available on the </span><br /><span style={{ fontWeight: 'bold', color: '#ffff' }}>Apple Store</span></p>
+                                        <p className='my-auto' style={{ fontSize: '8px',margin:'0px 5px' }}><span style={{ color: 'gray' }} >Available on the </span><br /><span style={{ fontWeight: 'bold', color: '#ffff' }}>Apple Store</span></p>
                                     </div>
 
 
