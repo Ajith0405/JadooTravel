327b65376f79ce8c0f99b80ee6eeb855381add40
AJITH S <128063551+Ajith0405@users.noreply.github.com>
2023-10-14 12:20:44 +0530

    third commit

--- src/Partners.js
+++ src/Partners.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './css/partners.css'
 import image27 from './images/image 27.png'
 import image28 from './images/image 28.png'
 import image29 from './images/image 29.png'
@@ -7,22 +8,22 @@ import image31 from './images/image 31.png'
 
 function Partners() {
   return (
-    <div className='container'>
+    <div className='container w-75 my-2 '>
         <div className='container d-flex justify-content-between align-items-center'>
-            <div>
-                <img src={image27} alt='axon'/>
+            <div className='options'>
+                <img className='options_img'  src={image27} alt='axon'/>
             </div>
-            <div>
-                <img src={image28} alt='axon'/>
+            <div className='options'>
+                <img className='options_img' src={image28} alt='axon'/>
             </div>
-            <div>
-                <img src={image29} alt='axon'/>
+            <div className='options'>
+                <img className='options_img' src={image29} alt='axon'/>
             </div>
-            <div>
-                <img src={image30} alt='axon'/>
+            <div className='options'>
+                <img className='options_img' style={{padding:'30px 0px'}} src={image30} alt='axon'/>
             </div>
-            <div>
-                <img src={image31} alt='axon'/>
+            <div className='options' >
+                <img className='options_img' style={{padding:'30px 0px'}} src={image31} alt='axon'/>
             </div>
         </div>
     </div>
