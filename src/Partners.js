b16f19ce13001dc680a265452f4217fd9f6a2c20
AJITH S <128063551+Ajith0405@users.noreply.github.com>
2023-10-14 11:04:37 +0530

    third commit

---
+++ src/Partners.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import image27 from './images/image 27.png'
+import image28 from './images/image 28.png'
+import image29 from './images/image 29.png'
+import image30 from './images/image 30.png'
+import image31 from './images/image 31.png'
+
+function Partners() {
+  return (
+    <div className='container'>
+        <div className='container d-flex justify-content-between align-items-center'>
+            <div>
+                <img src={image27} alt='axon'/>
+            </div>
+            <div>
+                <img src={image28} alt='axon'/>
+            </div>
+            <div>
+                <img src={image29} alt='axon'/>
+            </div>
+            <div>
+                <img src={image30} alt='axon'/>
+            </div>
+            <div>
+                <img src={image31} alt='axon'/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Partners
